2697837aa9f874653c862c585aea7a8051d91054
ub_iMac <datamaker@hud-on.com>
2016-10-20 09:34:07 +0900

    package 변경9

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "datamaker:naver-connect",
-  version: '0.0.4',
+  version: '0.0.5',
   summary: "Naver Connect package to connect naver users to existing users.",
   git: 'https://github.com/datamaker/meteor-naver-connect',
   documentation: 'README.md'
