27656e6247c7d37b9200fdbe8cea5fa7c1c869ff
ub_iMac <datamaker@hud-on.com>
2016-10-20 11:00:00 +0900

    package 변경15

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ var pinEncryptedFieldsToUser = function (serviceData, userId) {
 Accounts.updateOrCreateUserFromExternalService = function (serviceName, serviceData, options) {
 
     // 실명, 생일 삭제
-    delete options.name;
-    delete options.birthday;
+    delete options.profile.name;
+    delete options.profile.birthday;
 
     // naver는 options에 유저 정보가 있어서 serviceData로 데이터 이동
     serviceData = _.extend(serviceData, options.profile);
