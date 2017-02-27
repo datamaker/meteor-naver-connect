00df996218e9af764b6e4ddc17a7de2b7dd47724
권정빈 <datamaker@urbanbase.com>
2017-02-27 19:13:38 +0900

    권정빈 / 0.1.6 업그레이드

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: "datamaker:naver-connect",
-    version: '0.1.5',
+    version: '0.1.6',
     summary: "Naver Connect package to connect naver users to existing users.",
     git: 'https://github.com/datamaker/meteor-naver-connect',
     documentation: 'README.md'
@@ -8,12 +8,12 @@ Package.describe({
 
 
 Package.on_use(function (api) {
-    api.use(['spectrum:accounts-naver@0.0.1'], ['client', 'server']);
+    api.use(['datamaker:accounts-naver@0.0.1'], ['client', 'server']);
 
     //add dependency for overriding core
     api.use('oauth-encryption@1.2.0', 'server', {weak: true});
     api.use('oauth@1.1.13');
-    api.use('spectrum:naver@0.0.1', ['client', 'server']);
+    api.use('datamaker:naver@0.0.1', ['client', 'server']);
 
     api.add_files(["client.js"], 'client');
     api.add_files(["server.js"], 'server');
